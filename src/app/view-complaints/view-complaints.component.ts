aac5c6fef6a0232864feeca8ff6ecaf5139827d9
Michelle <msumalini@gmail.com>
2019-04-15 17:42:59 +0530

    can view uploaded image of the equipment that is in the corressponding complaint (customer)

--- src/app/view-complaints/view-complaints.component.ts
+++ src/app/view-complaints/view-complaints.component.ts
@@ -4,6 +4,7 @@ import {WebsocketService} from '../websocket.service';
 import { StoreStatusService} from '../store-status.service';
 import { element } from '@angular/core/src/render3';
 import Complaint from '../complaint';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-view-complaints',
@@ -12,17 +13,31 @@ import Complaint from '../complaint';
 })
 export class ViewComplaintsComponent implements OnInit {
 
+  index;
   elements: Complaint[];
+  imagePath
 
-  headElements = ['Equipment Name', 'Equipment Fault','Date','Technician Allocated','Image'];
+
+  headElements = ['Complaint Number','Equipment Name', 'Equipment Fault','Date','Technician Allocated','Image'];
 
 
   constructor(
     private customerService:CustomerServiceService,
     private storeStatusService:StoreStatusService,
+    private _sanitizer: DomSanitizer
   ) { }
 
+  setImageSrc(event)
+  {
+    var target=event.target || event.srcElement || event.currentTarget
+    var elementIndex=target.attributes.id.value;
+    var image = this.elements[elementIndex].image
+    this.imagePath = this._sanitizer.bypassSecurityTrustResourceUrl('data:'+image.filename+';base64,'+image.filevalue);
+
+  }
+
   ngOnInit() {
+    this.index=0;
     this.storeStatusService.setLoginStatus()
     this.customerService.viewCustomerComplaints(localStorage.getItem('userId'))
     .subscribe(complaints=>{
