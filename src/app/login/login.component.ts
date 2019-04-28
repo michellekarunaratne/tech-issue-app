c08e1f0a637717e864f924d63b85f87e2dafd79a
Michelle <msumalini@gmail.com>
2019-04-28 11:06:13 +0530

    added login form validators

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService} from '../login.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder,Validators  } from '@angular/forms';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import {WebsocketService} from '../websocket.service';
@@ -14,45 +14,56 @@ import { StoreStatusService} from '../store-status.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  submitted=false
   loginForm=this.fb.group({
-    userId:[''],
-    password:['']
+    userId:['',Validators.required],
+    password:['',Validators.required]
 
   });
 
+  get form() { return this.loginForm.controls; }
+
   onSubmit(){
 
-    //console.log(this.loginForm.value);
-    this.loginService.login(this.loginForm.value)
-    .subscribe(user =>{
-     
-      if(user.empId==null && user.nic.includes("v"))
-      {
-        localStorage.setItem('customer.firstName',user.firstName)
-        localStorage.setItem('customer.lastName',user.lastName)
-        localStorage.setItem('customer.email',user.email)
-        localStorage.setItem('customer.phone',user.phone.toString())
-        this.storeStatusService.setLoginStatus()
-        localStorage.setItem('userId',user.nic)
-        this.router.navigate(['/customerDash'])
-      }
-      else if(user.empId.includes("emp"))
-      {
-        localStorage.setItem('staff.firstName',user.firstName)
-        localStorage.setItem('staff.lastName',user.lastName)
-        localStorage.setItem('staff.email',user.email)
-        localStorage.setItem('staff.phone',user.phone.toString())
+    
+    if(this.loginForm.valid)
+    {
+        this.loginService.login(this.loginForm.value)
+        .subscribe(user =>{
         
-        this.storeStatusService.setLoginStatus()
-        localStorage.setItem('empId',user.empId)
-        this.webSocketService.logActiveStaffUser(user.empId)
-        this.router.navigate(['/staffDash'])
-      }
-      else
-      {
-        alert("User Not Found")
-      }
-    });
+          if(user.empId==null && user.nic.includes("v"))
+          {
+            localStorage.setItem('customer.firstName',user.firstName)
+            localStorage.setItem('customer.lastName',user.lastName)
+            localStorage.setItem('customer.email',user.email)
+            localStorage.setItem('customer.phone',user.phone.toString())
+            this.storeStatusService.setLoginStatus()
+            localStorage.setItem('userId',user.nic)
+            this.router.navigate(['/customerDash'])
+          }
+          else if(user.empId.includes("emp"))
+          {
+            localStorage.setItem('staff.firstName',user.firstName)
+            localStorage.setItem('staff.lastName',user.lastName)
+            localStorage.setItem('staff.email',user.email)
+            localStorage.setItem('staff.phone',user.phone.toString())
+            
+            this.storeStatusService.setLoginStatus()
+            localStorage.setItem('empId',user.empId)
+            this.webSocketService.logActiveStaffUser(user.empId)
+            this.router.navigate(['/staffDash'])
+          }
+          else
+          {
+            alert("User Not Found")
+          }
+        });
+    }
+    else if(this.loginForm.invalid)
+    {
+      this.submitted=true
+    }
   }
 
 
